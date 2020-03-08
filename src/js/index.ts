15a6d9d87ccd6175e49833e89d6298e5996e2e3a
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2020-03-08 14:33:48 +0100

    https://github.com/kami44/Calculator101

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,42 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let calculatateButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("calculateButton");
+calculatateButton.addEventListener("click", calculate);
+
+function calculate(): void{
+    let input1: HTMLInputElement = <HTMLInputElement>document.getElementById("number1");
+    let input2: HTMLInputElement = <HTMLInputElement>document.getElementById("number2");
+    let number1Str: string = input1.value;
+    let number2Str: string = input2.value;
+
+    let number1: number = Number(number1Str); 
+    let number2: number = Number(number2Str);
+
+    let operationElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("operation");
+    let operation: string = operationElement.value;
+    let result: number = 0;
+    result = calc(operation, number1, number2);
+
+    let resultElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("result");
+    resultElement.innerHTML = String(result);
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+function calc(operation: string, number1: number, number2: number): number{
+    let result: number = 0;
+    switch (operation){
+        case "+":
+            result = number1 + number2;
+            break;
+        case "-":
+            result = number1 - number2;
+            break;
+        case "*":
+            result = number1 * number2;
+            break;
+        case "/":
+            result = number1 / number2;
+            break;
+                    
+                
+    }
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    return result;
+}
